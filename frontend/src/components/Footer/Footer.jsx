486e20ea003e827f54baf81b698bd203b4d89568
Monu152415 <monalchilkoti@gmail.com>
2024-07-05 00:28:10 +0530

    first-commit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
             <img className='tomatologofooter' src={assets.logo} alt="" />
-            <p>This website is just for my portfolio, it's not a real website.</p>
+            <p>This website is just for my portfolio, THANKS FOR WATCHING.</p>
             <div className="footer-social-icons">
             <img src={assets.facebook_icon} alt="" />
               <img src={assets.twitter_icon} alt="" />
@@ -17,7 +17,7 @@ const Footer = () => {
             </div>
         </div>
         <div className="footer-content-center">
-            <h2>COMPANY</h2>
+            <h2>Monu Foods</h2>
             <ul>
               <li>Home</li>
               <li>About us</li>
@@ -28,13 +28,13 @@ const Footer = () => {
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-              <li>+1-212-456-7890</li>
-              <li>contact@tomato.com</li>
+              <li>956888xx5012</li>
+              <li>chilkotikartik@gmail.com</li>
             </ul>
         </div>
       </div>
       <hr/>
-      <p className='footer-copyright'>Copyright 2024 © Tomato.com - All rights reserved.</p>
+      <p className='footer-copyright'>Copyright 2024 © Chilkoti.com - All rights reserved.</p>
     </div>
   )
 }
